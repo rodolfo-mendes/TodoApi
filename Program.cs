0d8207797e8e86df291e2db93d3be50d96d9722a
rodolfo-mendes <rodolfo.m.s.mendes@gmail.com>
2021-12-13 14:59:09 -0300

    Controller scaffolding

--- Program.cs
+++ Program.cs
@@ -8,8 +8,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
 
-
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
